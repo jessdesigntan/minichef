791f0e9a561e6b48d52c3126fdcd396a2baf5e0f
Jess Tan <jess_tjl@hotmail.com>
2017-05-22 01:23:09 +0800

    profile pages

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 	var mcApp = angular.module('mcApp', ['ngRoute']);
 
+
 	mcApp.config(
     ['$routeProvider', function($routeProvider) {
 			$routeProvider.when('/', {
@@ -8,11 +9,40 @@
 				controller  : 'mainController'
 			})
 
-			// route for the about page
 			$routeProvider.when('/menu', {
 				title: 'Menu',
 				templateUrl : 'views/menu.html',
 				controller  : 'menuController'
+			})
+
+			$routeProvider.when('/menu-details', {
+				title: 'Menu Details',
+				templateUrl : 'views/menu-details.html',
+				controller  : 'menuDetailsController'
+			})
+
+			$routeProvider.when('/chef', {
+				title: 'Chef',
+				templateUrl : 'views/chef.html',
+				controller  : 'chefController'
+			})
+
+			$routeProvider.when('/profile', {
+				title: 'Profile',
+				templateUrl : 'views/profile.html',
+				controller  : 'profileController'
+			})
+
+			$routeProvider.when('/history', {
+				title: 'Reservation History',
+				templateUrl : 'views/history.html',
+				controller  : 'reservationHistoryController'
+			})
+
+			$routeProvider.when('/order', {
+				title: 'Order Details',
+				templateUrl : 'views/order.html',
+				controller  : 'orderController'
 			})
 
 			$routeProvider.otherwise({
@@ -28,11 +58,26 @@
 
 	// create the controller and inject Angular's $scope
 	mcApp.controller('mainController', function($scope) {
-		// create a message to display in our view
-		$scope.message = 'Everyone come and see how good I look!';
 		$scope.imgPath = 'images';
 	});
 
 	mcApp.controller('menuController', function($scope) {
-		$scope.message = 'Look! I am an about page.';
+	});
+
+	mcApp.controller('menuDetailsController', function($scope) {
+	});
+
+	mcApp.controller('chefController', function($scope) {
+	});
+
+	mcApp.controller('profileController', function($scope) {
+	});
+
+	mcApp.controller('reservationHistoryController', function($scope, $location) {
+		$scope.historyDetails = function() {
+		  $location.url('order');
+		};
+	});
+
+	mcApp.controller('orderController', function($scope) {
 	});
---
+++ js/directive/profileSidebar.js
@@ -0,0 +1,7 @@
+
+'use strict';
+module.exports = [function () {
+    return {
+      template : "<h1>Made by a directive!</h1>"
+    };
+}];
